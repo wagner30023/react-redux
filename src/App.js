0390ebfcc2db95571dc5e90d8cd874597c12d6dc
Carlos Wagner <wagner30023@gmail.com>
2022-10-10 12:49:53 -0300

    gerenciamento de estados com redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component  {
+  state = {
+    inputValue: ''
+  }
+
+  inputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  render() {
+    const {
+      clickButton,
+      newValue
+    } = this.props;
+
+    return (
+      <div className="App" style={{ paddingTop: '10px' }}>
+        <input 
+          onChange={this.inputChange}
+          type="text"
+          value={this.state.inputValue} 
+        />
+        <button onClick={() => clickButton(this.state.inputValue)}>
+          Click me
+        </button>
+        <h1>{newValue}</h1>
+      </div>
+    );
+  } 
 }
 
-export default App;
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clickButton }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
